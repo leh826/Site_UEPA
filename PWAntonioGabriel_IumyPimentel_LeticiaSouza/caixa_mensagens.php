14a78f5488659336a8250f72e69cb71608f0d2ce
Iumy Pimentel <117225271+Iuky-O@users.noreply.github.com>
2023-06-15 00:03:18 -0300

    Update caixa_mensagens.php

--- PWAntonioGabriel_IumyPimentel_LeticiaSouza/caixa_mensagens.php
+++ PWAntonioGabriel_IumyPimentel_LeticiaSouza/caixa_mensagens.php
@@ -10,14 +10,40 @@ $result = mysqli_query ($conn, $sql);
 <!DOCTYPE>
 <html>
     <hear>
-        <meta charset="UTF-8">
-        <title>Caixa de Mensagens</title>
-        <meta name="viewport" content="width=device-width">
-        <link rel="stylesheet" href="css/bootstrap.css">
-        <script src="js/valida.js" type="text/javascript"></script>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Universidade do Estado do Pará - Caixa de Mensagens</title>
+    <link rel="icon" href="./img/Logo Curso Sem fundo.png">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/rodape.css">
+    <link rel="stylesheet" href="./css/bootstrap.css">
+    <script src="js/valida.js" type="text/javascript"></script>
     </hear>
     
-    <body>
+    <body class="text-center">
+        
+       <header class="cabecalho">
+        <div>
+            <a href="./index.html"><img src="./img/Logo Curso Sem fundo.png" alt="Foto da Logo do Curso de Ciência de Dados"></a>
+        </div>
+
+        <div>
+            <nav class="navegacao">
+                <ul>
+                    <li><a href="matrizcurricular.html" >Matriz Curricular</a></li>
+                    <li><a href="sistavaliacao.html">Sistema de Avaliação</a></li>
+                    <li><a href="corpodocente.html">Corpo Docente</a></li>
+                    <li><a href="campus.html">Estrutura Campus</a></li>
+                    <li><a href="colegiado.html">Colegiado</a></li>
+                    <li><a href="nucleodocente.html">Núcleo Docente</a></li>
+                    <li><a href="fale_conosco.html">Fale Conosco</a></li>
+                </ul>
+            </nav>
+        </div>
+   </header>
         
         <div class="jumbotron">
             <div class="container">
@@ -51,6 +77,19 @@ $result = mysqli_query ($conn, $sql);
                     
             </table>
         </div>
+
+        <footer class="footer">
+            <!-- Copyright -->
+            <div class="text-center p-3 text-dark">
+            © 2023 Copyright:
+            <h3 class="text-dark">Antônio Gabriel | Iumy Pimentel | Leticia Souza </h3>
+            </div>
+            <!-- Copyright -->
+        </footer>
+    
+   <!--div class="rodape">
+        <h3>Desenvolvido por &copy; Antônio Gabriel | Iumy Pimentel | Leticia Souza </h3>
+   </div>-->
         
         <script src="../PW2_AntonioGabriel_IumyPimentel_LeticiaJuliana/js/valida.js"></script>
     </body>
